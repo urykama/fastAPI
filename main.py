d4cb690a55da2d5846ef7ae7bf853e2e6e4973a6
UryKama <urykama@gmail.com>
2022-10-13 10:13:21 +0500

    lesson2_+def get_item

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
-
 @app.get('/')
 def home():
-    return {"key":"Hello"}
+    return {"key": "Hello"}
 
+
+@app.get('/{pk}')
+def get_item(pk: int, q: str = None):
+    return {"key": pk, "q": q}
 # uvicorn main:app --reload --port 8080
